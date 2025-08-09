7567a02ec9ce9d2dc202b0bf681348fb8aab7dfa
kenkatou354 <kenkatou354@gmail.com>
2025-08-10 02:56:07 +0900

    [add]インプットシステムのアクション定義

--- Assets/Buffer/InputBuffer.cs
+++ Assets/Buffer/InputBuffer.cs
@@ -1,6 +1,26 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputBuffer : MonoBehaviour
+namespace Runtime
 {
-  
+    /// <summary>
+    /// 入力をバッファリングするクラス
+    /// </summary>
+    [RequireComponent (typeof (PlayerInput))]
+    public class InputBuffer : MonoBehaviour
+    {
+        private const string MOVE_ACTION = "Move";
+        private const string SPRINT_ACTION = "Sprint";
+        private const string CARRY_ACTION = "Carry";
+       
+        public Vector2 Move { get;private set; }
+        public bool Sprint { get; private set; }
+
+        private InputAction _moveAction;
+        private InputAction _sprintAction;
+        private InputAction _carryAction;
+        
+    }
 }
+
