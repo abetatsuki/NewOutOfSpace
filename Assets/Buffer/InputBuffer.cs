ffd6d9ec10b1b13b2b4dcebf22877bf875fc7474
kenkatou354 <kenkatou354@gmail.com>
2025-08-10 03:00:12 +0900

    [add]バッファークラスで入力を受け取る処理を追加

--- Assets/Buffer/InputBuffer.cs
+++ Assets/Buffer/InputBuffer.cs
@@ -1,26 +1,33 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+[RequireComponent(typeof(PlayerInput))]
+[DefaultExecutionOrder(-1000)]
 
-namespace Runtime
+public class InputBuffer : MonoBehaviour
 {
-    /// <summary>
-    /// 入力をバッファリングするクラス
-    /// </summary>
-    [RequireComponent (typeof (PlayerInput))]
-    public class InputBuffer : MonoBehaviour
-    {
-        private const string MOVE_ACTION = "Move";
-        private const string SPRINT_ACTION = "Sprint";
-        private const string CARRY_ACTION = "Carry";
-       
-        public Vector2 Move { get;private set; }
-        public bool Sprint { get; private set; }
+    private const string MOVE_ACTION = "Move";
+    private const string SPRINT_ACTION = "Sprint";
+    private const string CARRY_ACTION = "Carry";
+   
 
-        private InputAction _moveAction;
-        private InputAction _sprintAction;
-        private InputAction _carryAction;
-        
+    public InputAction MoveAction => _moveAction;
+    public InputAction SprintAction => _sprintAction;
+    public InputAction CarryAction => _carryAction;
+
+
+    private InputAction _moveAction;
+    private InputAction _sprintAction;
+    private InputAction _carryAction;
+
+
+    private void Awake()
+    {
+        if (TryGetComponent<PlayerInput>(out var playerInput))
+        {
+            _moveAction = playerInput.actions[MOVE_ACTION];
+            _sprintAction = playerInput.actions[SPRINT_ACTION];
+            _carryAction = playerInput.actions[CARRY_ACTION];
+
+        }
     }
 }
-
