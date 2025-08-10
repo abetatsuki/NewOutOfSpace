9d4c1e5d613434733f8380b5dabc7a73ba249d05
kenkatou354 <kenkatou354@gmail.com>
2025-08-10 12:03:51 +0900

    [add]入力を受けとったら通知するクラスを追加　

---
+++ Assets/Buffer/PlayerInputNotifier.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerInputNotifier : MonoBehaviour
+{
+    private const string MOVE_ACTION = "Move";
+    private const string SPRINT_ACTION = "Sprint";
+    private const string CARRY_ACTION = "Carry";
+
+    public event Action<Vector2> OnMove;   // 移動入力時通知
+    public event Action<bool> OnSprint;    // スプリント状態通知
+    public event Action OnCarry;           // キャリー入力通知
+
+    private InputAction moveAction;
+    private InputAction sprintAction;
+    private InputAction carryAction;
+
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        moveAction = playerInput.actions[MOVE_ACTION];
+        sprintAction = playerInput.actions[SPRINT_ACTION];
+        carryAction = playerInput.actions[CARRY_ACTION];
+    }
+
+    private void OnEnable()
+    {
+        // MOVEアクション
+        moveAction.performed += ctx => OnMove?.Invoke(ctx.ReadValue<Vector2>());
+        moveAction.canceled += ctx => OnMove?.Invoke(Vector2.zero);
+
+        // SPRINTアクション
+        sprintAction.performed += ctx => OnSprint?.Invoke(true);
+        sprintAction.canceled += ctx => OnSprint?.Invoke(false);
+
+        // CARRYアクション
+        carryAction.performed += ctx => OnCarry?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.performed -= ctx => OnMove?.Invoke(ctx.ReadValue<Vector2>());
+        moveAction.canceled -= ctx => OnMove?.Invoke(Vector2.zero);
+
+        sprintAction.performed -= ctx => OnSprint?.Invoke(true);
+        sprintAction.canceled -= ctx => OnSprint?.Invoke(false);
+
+        carryAction.performed -= ctx => OnCarry?.Invoke();
+    }
+}
