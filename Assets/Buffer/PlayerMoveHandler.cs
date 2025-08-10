ce79739fe5a265842e6507a67e769d023f2c0e6e
kenkatou354 <kenkatou354@gmail.com>
2025-08-10 13:12:35 +0900

    [add]プレイヤーの動きの実装

--- Assets/Buffer/PlayerMoveHandler.cs
+++ Assets/Buffer/PlayerMoveHandler.cs
@@ -2,5 +2,31 @@ using UnityEngine;
 
 public class PlayerMoveHandler : MonoBehaviour
 {
-  
+    [SerializeField] private PlayerInputNotifier inputNotifier;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector3 moveDirection;
+    private void OnEnable()
+    {
+        inputNotifier.OnMove += HandleMove;
+    }
+
+    private void OnDisable()
+    {
+        inputNotifier.OnMove -= HandleMove;
+    }
+
+    private void HandleMove(Vector2 input)
+    {
+         moveDirection = new Vector3(input.x, 0f, input.y);  // YはZにマッピング
+                                                                    // ここでmoveDirectionを使う（XZ平面移動）
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = moveDirection * moveSpeed;
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z); // Y軸の速度は維持
+    }
 }
