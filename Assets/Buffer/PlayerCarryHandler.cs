0f3ecb33c5cd4a4ca22e49618a0cbb63121ad898
kenkatou354 <kenkatou354@gmail.com>
2025-08-10 13:11:58 +0900

    [add]テスト用スクリプトを実装

--- Assets/Buffer/PlayerCarryHandler.cs
+++ Assets/Buffer/PlayerCarryHandler.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class PlayerCarryHandler : MonoBehaviour
 {
-    
+    [SerializeField] private PlayerInputNotifier inputNotifier;
+
+    private void OnEnable()
+    {
+        inputNotifier.OnCarry += HandleCarry;
+    }
+
+    private void OnDisable()
+    {
+        inputNotifier.OnCarry -= HandleCarry;
+    }
+
+    private void HandleCarry()
+    {
+        Debug.Log("キャリー処理");
+        // キャリーアクションの実装
+    }
 }
--- Assets/Buffer/PlayerSprintHandler.cs
+++ Assets/Buffer/PlayerSprintHandler.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class PlayerSprintHandler : MonoBehaviour
 {
-    
+    [SerializeField] private PlayerInputNotifier inputNotifier;
+
+    private void OnEnable()
+    {
+        inputNotifier.OnSprint += HandleSprint;
+    }
+
+    private void OnDisable()
+    {
+        inputNotifier.OnSprint -= HandleSprint;
+    }
+
+    private void HandleSprint(bool isSprinting)
+    {
+        Debug.Log("スプリント処理: " + isSprinting);
+        // スプリント時の速度変更など
+    }
 }
