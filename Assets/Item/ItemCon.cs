5c1ab75b56ad1509103b96000c88d4f1c84eeb0f
kenkatou354 <kenkatou354@gmail.com>
2025-08-19 21:27:18 +0900

    [Add]ステータスが変わったら通知を受け取る機能を追加

--- Assets/Item/ItemCon.cs
+++ Assets/Item/ItemCon.cs
@@ -5,5 +5,22 @@ public class ItemCon : MonoBehaviour
     [SerializeField]private ItemData _item;
     [SerializeField]private PlayerInputNotifier _playerInputNotifier;
     [SerializeField]private PlayerStatusBuffer _playerStatusBuffer;
+    
+
+    private void OnEnable()
+    {
+     
+        _playerStatusBuffer.OnCarryStatusChanged += OnCarryStatusChanged;
+    }
+    private void OnDisable()
+    {
+
+        _playerStatusBuffer.OnCarryStatusChanged -= OnCarryStatusChanged;
+    }
+    
+    public void OnCarryStatusChanged(CarryStatus carrystatus)
+    {
+
+    }
 
 }
