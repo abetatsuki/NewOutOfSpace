b74b5dca249bbdec2823e41c55cc83dbab6c29f6
kenkatou354 <kenkatou354@gmail.com>
2025-08-19 21:08:09 +0900

    [Add]必要なデータをシリアライズ化

--- Assets/Item/ItemCon.cs
+++ Assets/Item/ItemCon.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class ItemCon : MonoBehaviour
 {
-    
+    [SerializeField]ItemData item;
+    [SerializeField]PlayerInputNotifier playerInputNotifier;
+    [SerializeField]PlayerStatusBuffer playerStatusBuffer;
 }
