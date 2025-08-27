7b5abf027af2a34578bc45dc16cdb9e2da65f2b5
kenkatou354 <kenkatou354@gmail.com>
2025-08-28 04:34:06 +0900

    [Add]button

--- Assets/SceneLoad/SceneControlButton.cs
+++ Assets/SceneLoad/SceneControlButton.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneControlButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    enum TargetScene
     {
-        
+        Next,
+        Previous,
+        MainMenu
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] TargetScene targetscene;
+    Button button;
+
+    private void Start()
     {
-        
+        button = GetComponent<Button>();
+
+        button.onClick.RemoveAllListeners();
+        switch (targetscene)
+        {
+            case TargetScene.MainMenu:
+                button.onClick.AddListener(() => SceneController.LoadMainScene()); break;
+
+            case TargetScene.Next:
+                button.onClick.AddListener(() => SceneController.LoadNextScene()); break;
+
+            case TargetScene.Previous:
+                button.onClick.AddListener(() => SceneController.LoadPreviousScene()); break;
+        }
+
     }
 }
