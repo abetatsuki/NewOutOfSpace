740f7ef8e5319ac9ec08b4791a8e8498e25451ea
kenkatou354 <kenkatou354@gmail.com>
2025-08-28 04:40:08 +0900

    [Add[シーンローダーの完成

--- Assets/SceneLoad/SceneController.cs
+++ Assets/SceneLoad/SceneController.cs
@@ -1,33 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public  static class SceneController 
+
+public static class SceneController
 {
-    static int  mainScene = 0 ;
- public static void LoadMainScene()
+    static int mainScene = 0;
+
+    // メインシーンをロード
+    public static void LoadMainScene()
     {
         SceneManager.LoadScene(mainScene);
     }
+
+    // 次のシーンをロード
     public static void LoadNextScene()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
 
-        if(currentScene  < SceneManager.sceneCountInBuildSettings)
-            SceneManager.LoadScene(currentScene + 1 );
+        if (currentScene < SceneManager.sceneCountInBuildSettings - 1)
+            SceneManager.LoadScene(currentScene + 1);
+        else
+            Debug.Log("これ以上次のシーンはありません。");
     }
 
+    // 前のシーンをロード
     public static void LoadPreviousScene()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
 
         if (currentScene > 0)
-            SceneManager.LoadScene(currentScene + 1);
+            SceneManager.LoadScene(currentScene - 1);
+        else
+            Debug.Log("これ以上前のシーンはありません。");
     }
 
+    // 任意のシーンをロード
     public static void LoadScene(int index)
     {
-        
-
-        if (index>= 0&& index < SceneManager.sceneCountInBuildSettings)
+        if (index >= 0 && index < SceneManager.sceneCountInBuildSettings)
             SceneManager.LoadScene(index);
+        else
+            Debug.LogError("シーン番号が範囲外です: " + index);
     }
 }
