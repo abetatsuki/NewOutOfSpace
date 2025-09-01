62c0e8487de7e44b7cc25173d5b43e6628c6abc4
kenkatou354 <kenkatou354@gmail.com>
2025-09-01 18:55:33 +0900

    [Add]ショップのボタン処理を追加

--- Assets/Shop/ShopButton.cs
+++ Assets/Shop/ShopButton.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopButton : MonoBehaviour
 {
-   
+   enum TargetShopScene
+    {
+        ShopOpen,
+        ShopBuy,
+        ShopSell,
+
+
+    }
+    [SerializeField] TargetShopScene targetShopScene;
+    Button Button;
+
+    private void Start()
+    {
+        Button = GetComponent<Button>();
+        Button.onClick.RemoveAllListeners();
+        switch (targetShopScene)
+        {
+            case TargetShopScene.ShopOpen:
+                Debug.Log("Open");
+                break;
+                case TargetShopScene.ShopBuy:
+                Debug.Log("Buy");
+                break;
+                case TargetShopScene.ShopSell:
+                Debug.Log("sell");
+                break;
+        }
+    }
 }
