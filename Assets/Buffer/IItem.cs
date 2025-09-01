cd64a5424ce69c0bc08df157047b2b42b42759a7
kenkatou354 <kenkatou354@gmail.com>
2025-09-01 13:10:13 +0900

    [Add]使う、置くアイテムのインターフェイスを追加

---
+++ Assets/Buffer/IItem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+interface UseImte //左クリックで使用できるitem
+{
+    void Use();
+}
+interface PutItem //置くだけで効果があるitem
+{
+    void Put();
+}
+
+public class IItem : MonoBehaviour
+{
+    
+}
